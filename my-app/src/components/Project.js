88b8d3c6086a4d8202ce49706c70bdb7e95631fc
Shivesh Nandan <kshivesh929@gmail.com>
2024-06-20 11:44:28 +0530

    new

--- my-app/src/components/Project.js
+++ my-app/src/components/Project.js
@@ -9,25 +9,7 @@ export default function Project() {
     let offset = window.pageYOffset;
     parallax2.style.backgroundPositionY = offset * -0.0001 + "px";
 });
-  // document.querySelector(document)
-  // .addEventListener("mousemove", ".card", function(event) {
-  //   var halfWidth = (this.clientWidth / 2);
-  //   var halfHeight = (this.clientHeight / 2);
-  //   var mouseX = (halfWidth - (event.pageX - this.offsetLeft));
-  //   var mouseY = (halfHeight - (event.pageY - this.offsetTop));
-  //   var degX  = ((mouseY / halfHeight) * 15) + 'deg'; //max degree = 15
-  //   var degY  = ((mouseX / halfWidth) * -15) + 'deg'; //max degree = 15
-  //   document.querySelector(this).css('transform', function() {
-  //     return 'perspective(512px) translate3d(-2px, -2px, 0) rotateX('+ degX +') rotateY('+ degY +')';
-  //   })
-  //   .children('.card__info').css('transform', function() {
-  //     return 'perspective(512px) translate3d(4px, 4px, 0) rotateX('+ degX +') rotateY('+ degY +')';
-  //   });
-  // })
-  // .addEventListener("mouseout", ".card", function() {
-  //   document.querySelector(this).removeAttr('style')
-  //   .children('.card__info').removeAttr('style');
-  // });
+  
 
   return (
     <>
