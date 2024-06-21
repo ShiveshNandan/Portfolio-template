1bf4e42f0c4d23ba25b667fedb8cf35e8918b638
Shivesh Nandan <kshivesh929@gmail.com>
2024-06-21 10:59:11 +0530

    something

--- portfolio/my-app/src/components/UI Componemts/Modal.tsx
+++ portfolio/my-app/src/components/UI Componemts/Modal.tsx
@@ -8,7 +8,7 @@ interface ModalInterface {
 const Modal: React.FC<ModalInterface> = ({ Heading, onClose, children }) => {
   return (
     <>
-      <div className="flex flex-col fixed backdrop-blur-[16px] top-0 left-0 w-full h-screen">
+      <div className=" backdrop-blur1 flex flex-col fixed  top-0 left-0 w-full h-screen">
         <div className="w-11/12 bg-[#0a0a0a99] backdrop-blur-[66px] m-auto  min-h-[40vh] max-h-[80vh] rounded-xl border ">
           <div className="flex justify-between py-3 px-4 text-2xl border-b backdrop-blur-[16px]">
             <h1>{Heading}</h1>
